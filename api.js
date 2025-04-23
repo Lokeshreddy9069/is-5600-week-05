162d460c50520a524ed38b80841ad1ca5339d238
Lokeshreddy9069 <jangalokeshreddy@gmail.com>
2025-04-23 03:55:53 +0000

    Update README.md

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const Products = require('./products')
+const Orders = require('./orders')
 const autoCatch = require('./lib/auto-catch')
 
 /**
@@ -52,6 +53,40 @@ async function getProduct(req, res, next) {
 async function createProduct(req, res) {
   console.log('request body:', req.body)
   res.json(req.body)
+async function createProduct (req, res, next) {
+  const product = await Products.create(req.body)
+  res.json(product)
+}
+  
+  
+  
+async function editProduct (req, res, next) {
+  const change = req.body
+  const product = await Products.edit(req.params.id, change)
+  res.json(product)
+}
+  
+async function deleteProduct (req, res, next) {
+  const response = await Products.destroy(req.params.id)
+  res.json(response)
+}
+  
+async function createOrder (req, res, next) {
+  const order = await Orders.create(req.body)
+  res.json(order)
+}
+  
+async function listOrders (req, res, next) {
+  const { offset = 0, limit = 25, productId, status } = req.query
+  
+  const orders = await Orders.list({ 
+   offset: Number(offset), 
+   limit: Number(limit),
+   productId, 
+   status 
+})
+  
+  res.json(orders)
 }
 
 /**
@@ -63,6 +98,10 @@ async function createProduct(req, res) {
 async function editProduct(req, res, next) {
   console.log(req.body)
   res.json(req.body)
+async function editOrder(req, res, next) {
+  const updatedOrder = await Orders.edit(req.params.id, req.body)
+  if (!updatedOrder) return res.status(404).json({ error: "Order not found" })
+  res.json(updatedOrder)
 }
 
 /**
@@ -73,6 +112,9 @@ async function editProduct(req, res, next) {
  */
 async function deleteProduct(req, res, next) {
   res.json({ success: true })
+async function deleteOrder(req, res, next) {
+  await Orders.destroy(req.params.id)
+  res.status(204).send()
 }
 
 module.exports = autoCatch({
@@ -81,5 +123,9 @@ module.exports = autoCatch({
   getProduct,
   createProduct,
   editProduct,
-  deleteProduct
+  deleteProduct,
+  createOrder,
+  listOrders,
+  editOrder,
+  deleteOrder
 });
